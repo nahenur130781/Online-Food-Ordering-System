047c8d15a59a546587b69094633e01e4f9ee80db
Umme Sharah <ummesharah7@gmail.com>
2025-01-03 23:58:07 +0600

    2nd update done

--- food.cpp
+++ food.cpp
@@ -37,3 +37,63 @@ public:
             getline(cin, customerName);
         }
     } // nahenur push
+void displayMenu() {
+        cout << "\n\t\t\t---------- HUNGER -----------\n\n";
+        cout << "Hello " << customerName << "!\nWhat would you like to order?\n\n";
+        cout << "\t\t\t-------- Menu --------\n\n";
+        cout << "1) Pizzas\n";
+        cout << "2) Burgers\n";
+        cout << "3) Sandwiches\n";
+        cout << "4) Rolls\n";
+        cout << "5) Biryani\n";
+    }
+
+    void displayItems(const vector<MenuItem> &items) {
+        for (size_t i = 0; i < items.size(); i++) {
+            cout << i + 1 << ") " << items[i].name << " (Tk." << items[i].price << ")\n";
+        }
+    }
+
+    void processOrder(int choice) {
+        int itemChoice, quantity;
+        string category;
+        vector<MenuItem> selectedMenu;
+
+        if (choice == 1)
+        {
+            category = "Pizza";
+            selectedMenu = pizzas;
+        }
+        else if (choice == 2) 
+        {
+            category = "Burger";
+            selectedMenu = burgers;
+        }
+        else if (choice == 3) 
+        {
+            category = "Sandwich";
+            selectedMenu = sandwiches;
+        }
+        else if (choice == 4) 
+        {
+            category = "Roll";
+            selectedMenu = rolls;
+        }
+        else if (choice == 5) 
+        {
+            category = "Biryani";
+            selectedMenu = biryanis;
+        }
+
+        cout << "\nAvailable " << category << " options:\n";
+        displayItems(selectedMenu);
+
+        cout << "\nPlease select an option: ";
+        while (!(cin >> itemChoice) || itemChoice < 1 || itemChoice > selectedMenu.size()) {
+            cout << "Invalid input. Please select a valid option: ";
+        }
+
+        cout << "\nEnter quantity: ";
+        while (!(cin >> quantity) || quantity <= 0) {
+            cout << "Invalid input. Please enter a valid quantity: ";
+        } // Alma push
